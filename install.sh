45a6706a1258ab4ee7c213189b920ebac7f7c35c
Yin Mingming <ymmtommy@163.com>
2014-12-17 06:22:33 +0000

    my script to install my tools on linux

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get upgrade 
+
+sudo apt-get install git git-core
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone http://github.com/ymm000596/vim.git
+
+sudo apt-get install libasound-dev libzmq-dev libfftw3-dev
+sudo apt-get install ctags cscope htop iftop iotop netcat
+
+sudo apt-get install python-numpy python-scipy python-pip python-zmq python-pygame python-pyaudio python-serial python-matplotlib python-guiqwt python-pandas ipython ipython-notebook python-dev
+
+sudo apt-get install lua5.1 lu5.1-dev
